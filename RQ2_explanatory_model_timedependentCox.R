cf51a833c8c943b9f7adece8d7283c5953f6c6e1
user1803158 <101558304+user1803158@users.noreply.github.com>
2023-12-06 11:23:00 +0100

    Update RQ2_explanatory_model_timedependentCox.R

--- RQ2_explanatory_model_timedependentCox.R
+++ RQ2_explanatory_model_timedependentCox.R
@@ -2,6 +2,9 @@
 ### prepare environment ###
 ###########################
 
+# load data
+source('C:/Users/lanbro/Documents/Scripts/data_management_RQ2.R')
+
 # load libraries 
 library(survival) # to fit the Cox model
 
@@ -11,24 +14,24 @@ library(survival) # to fit the Cox model
 
 # dataframe with only the necessary survival data 
 survdata <- left_join(
-  distinct(long_meas_train, id, patientId, PSA, NLCB, months_in_followup),
+  distinct(long_meas_train, id, patientId, PSA, NLCB, time),
   distinct(baseline_data_table1_train, patientId, therapy_received, NLCB_overall, 
            NLCB_overall_num, time_obs),
   by = 'patientId'
 ) %>%
   mutate(therapy_received = as.character(therapy_received)) %>%
-  arrange(patientId, months_in_followup)
+  arrange(patientId, time)
 
 # create the tstart variable 
 for(i in 1:nrow(survdata)){
-  if(survdata[i, 'months_in_followup'] == 0) survdata[i, 'tstart'] = 0
+  if(survdata[i, 'time'] == 0) survdata[i, 'tstart'] = 0
   else if(survdata[i-1, 'patientId'] == survdata[i, 'patientId']){
-    survdata[i, 'tstart'] = survdata[i-1, 'months_in_followup']
+    survdata[i, 'tstart'] = survdata[i-1, 'time']
   } 
 }
 
 # time-dependent Cox
-coxmodel <- coxph(Surv(tstart, months_in_followup, NLCB) ~ therapy_received + 
+coxmodel <- coxph(Surv(tstart, time, NLCB) ~ therapy_received + 
                     log(PSA + 0.01, 2), 
       data = survdata,
       id = patientId)
