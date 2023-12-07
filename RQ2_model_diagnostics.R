acda9aede947b65f6737612b11edee4d82f37924
user1803158 <101558304+user1803158@users.noreply.github.com>
2023-12-07 17:27:28 +0100

    Update RQ2_model_diagnostics.R

--- RQ2_model_diagnostics.R
+++ RQ2_model_diagnostics.R
@@ -6,7 +6,7 @@
 source('C:/Users/lanbro/Documents/Scripts/data_management_RQ2.R')
 
 # load models 
-source('C:/Users/lanbro/Documents/Scripts/RQ2_explanatory/explanatory_model_JMbayes_v22.R')
+source('C:/Users/lanbro/Documents/Scripts/RQ2_explanatory/explanatory_model_JMbayes_v24.R')
 models <- list(model1_longit, model2_longit, model3_longit, model4_longit,
                model5_longit)
 
@@ -30,9 +30,9 @@ outliers.Cox <- ggcoxdiagnostics(model_surv, type = 'deviance',
 linrel <- vector(mode = 'list', length = 5)
 
 for(i in 1:5){
-  linrel[[i]] <- ggplot(data.frame(months_in_followup = models[[i]]$data$months_in_followup, 
+  linrel[[i]] <- ggplot(data.frame(time = models[[i]]$data$time, 
                                    pearson = residuals(models[[i]],type = 'pearson')),
-                        aes(x = months_in_followup,y = pearson)) +
+                        aes(x = time,y = pearson)) +
     geom_point() +
     theme_bw() +
     xlab('Time in follow-up (months)') + 
@@ -89,7 +89,7 @@ set.seed(31102023)
 random.pts <- sample(unique(psa_long_train$id_num), 4)
 
 plots_longit.df <- psa_long_train %>%
-  select(id_num, therapy_received, months_in_followup, log2PSA) %>%
+  select(id_num, therapy_received, time, log2PSA) %>%
   cbind(fitted(model1_longit)) %>%
   cbind(fitted(model2_longit)) %>%
   cbind(fitted(model3_longit)) %>%
@@ -102,28 +102,29 @@ plots_longit.df <- psa_long_train %>%
          log2PSA.pred4 = 'fitted(model4_longit)',
          log2PSA.pred5 = 'fitted(model5_longit)')
 
-pt_sample_model_fits <- ggplot(plots_longit.df, aes(x = months_in_followup, y = log2PSA)) +
+pt_sample_model_fits <- ggplot(plots_longit.df, aes(x = time, y = log2PSA)) +
   facet_wrap(~ id_num) +
   geom_point() +
   geom_line() +
   geom_line(data = plots_longit.df, 
-            mapping = aes(x = months_in_followup, y = log2PSA.pred1, 
+            mapping = aes(x = time, y = log2PSA.pred1, 
                           color = 'linear', linetype = 'linear2')) +
   geom_line(data = plots_longit.df, 
-            mapping = aes(x = months_in_followup, y = log2PSA.pred5, 
+            mapping = aes(x = time, y = log2PSA.pred5, 
                           color = 'quadratic', linetype = 'quadratic2')) +
   geom_line(data = plots_longit.df, 
-            mapping = aes(x = months_in_followup, y = log2PSA.pred2, 
+            mapping = aes(x = time, y = log2PSA.pred2, 
                           color = 'ns with 1 knot', linetype = '1knot2')) +
   geom_line(data = plots_longit.df, 
-            mapping = aes(x = months_in_followup, y = log2PSA.pred3, 
+            mapping = aes(x = time, y = log2PSA.pred3, 
                           color = 'ns with 2 knots', linetype = '2knot2')) +
   geom_line(data = plots_longit.df, 
-            mapping = aes(x = months_in_followup, y = log2PSA.pred4, 
+            mapping = aes(x = time, y = log2PSA.pred4, 
                           color = 'ns with 3 knots', linetype = '3knot2')) +
   theme_bw() +
   xlab('Time in follow-up (months)') +
   ylab(expression(paste(log[2](PSA), phantom(x),'(ng/ml)'))) + 
+  xlim(c(0, 20)) +
   theme(legend.position = 'bottom') +
   scale_color_manual(labels=c('linear', 'quadratic', 'ns with 1 knot', 'ns with 2 knots',
                               'ns with 3 knots'),
@@ -148,59 +149,59 @@ rm(random.pts, plots_longit.df)
 
 # mean trajectories per treatment, split on mixed model
 fitted_per_treat_ARSi <- data.frame(
-  months_in_followup = seq(0, 35.5, 0.01),
-  ARSi1 = predict(model1_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 35.5, 0.01),
+  time = seq(0, 35.5, 0.01),
+  ARSi1 = predict(model1_longit, re.form = NA, newdata = data.frame(time = seq(0, 35.5, 0.01),
                                                                     therapy_received = 'ARSi'), level = 0),
-  ARSi2 = predict(model2_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 35.5, 0.01),
+  ARSi2 = predict(model2_longit, re.form = NA, newdata = data.frame(time = seq(0, 35.5, 0.01),
                                                                     therapy_received = 'ARSi'), level = 0),
-  ARSi3 = predict(model3_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 35.5, 0.01),
+  ARSi3 = predict(model3_longit, re.form = NA, newdata = data.frame(time = seq(0, 35.5, 0.01),
                                                                     therapy_received = 'ARSi'), level = 0),
-  ARSi4 = predict(model4_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 35.5, 0.01),
+  ARSi4 = predict(model4_longit, re.form = NA, newdata = data.frame(time = seq(0, 35.5, 0.01),
                                                                     therapy_received = 'ARSi'), level = 0),
-  ARSi5 = predict(model5_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 35.5, 0.01),
+  ARSi5 = predict(model5_longit, re.form = NA, newdata = data.frame(time = seq(0, 35.5, 0.01),
                                                                     therapy_received = 'ARSi'), level = 0),
   therapy_received = 'ARSi') 
 fitted_per_treat_Taxane <- data.frame(
-  months_in_followup = seq(0, 21.5, 0.01),
-  Taxane1 = predict(model1_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 21.5, 0.01),
+  time = seq(0, 21.5, 0.01),
+  Taxane1 = predict(model1_longit, re.form = NA, newdata = data.frame(time = seq(0, 21.5, 0.01),
                                                                       therapy_received = 'Taxane'), level = 0),
-  Taxane2 = predict(model2_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 21.5, 0.01),
+  Taxane2 = predict(model2_longit, re.form = NA, newdata = data.frame(time = seq(0, 21.5, 0.01),
                                                                       therapy_received = 'Taxane'), level = 0),
-  Taxane3 = predict(model3_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 21.5, 0.01),
+  Taxane3 = predict(model3_longit, re.form = NA, newdata = data.frame(time = seq(0, 21.5, 0.01),
                                                                       therapy_received = 'Taxane'), level = 0),
-  Taxane4 = predict(model4_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 21.5, 0.01),
+  Taxane4 = predict(model4_longit, re.form = NA, newdata = data.frame(time = seq(0, 21.5, 0.01),
                                                                       therapy_received = 'Taxane'), level = 0),
-  Taxane5 = predict(model5_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 21.5, 0.01),
+  Taxane5 = predict(model5_longit, re.form = NA, newdata = data.frame(time = seq(0, 21.5, 0.01),
                                                                       therapy_received = 'Taxane'), level = 0),
   therapy_received = 'Taxane') 
 fitted_per_treat_PARPi <- data.frame(
-  months_in_followup = seq(0, 16, 0.01),
-  PARPi1 = predict(model1_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 16, 0.01),
+  time = seq(0, 16, 0.01),
+  PARPi1 = predict(model1_longit, re.form = NA, newdata = data.frame(time = seq(0, 16, 0.01),
                                                                      therapy_received = 'PARPi'), level = 0),
-  PARPi2 = predict(model2_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 16, 0.01),
+  PARPi2 = predict(model2_longit, re.form = NA, newdata = data.frame(time = seq(0, 16, 0.01),
                                                                      therapy_received = 'PARPi'), level = 0),
-  PARPi3 = predict(model3_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 16, 0.01),
+  PARPi3 = predict(model3_longit, re.form = NA, newdata = data.frame(time = seq(0, 16, 0.01),
                                                                      therapy_received = 'PARPi'), level = 0),
-  PARPi4 = predict(model4_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 16, 0.01),
+  PARPi4 = predict(model4_longit, re.form = NA, newdata = data.frame(time = seq(0, 16, 0.01),
                                                                      therapy_received = 'PARPi'), level = 0),
-  PARPi5 = predict(model5_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 16, 0.01),
+  PARPi5 = predict(model5_longit, re.form = NA, newdata = data.frame(time = seq(0, 16, 0.01),
                                                                      therapy_received = 'PARPi'), level = 0),
   therapy_received = 'PARPi') 
 fitted_per_treat_Platinum <- data.frame(
-  months_in_followup = seq(0, 12, 0.01),
-  Platinum1 = predict(model1_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 12, 0.01),
+  time = seq(0, 12, 0.01),
+  Platinum1 = predict(model1_longit, re.form = NA, newdata = data.frame(time = seq(0, 12, 0.01),
                                                                         therapy_received = 'Platinum'), level = 0),
-  Platinum2 = predict(model2_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 12, 0.01),
+  Platinum2 = predict(model2_longit, re.form = NA, newdata = data.frame(time = seq(0, 12, 0.01),
                                                                         therapy_received = 'Platinum'), level = 0),
-  Platinum3 = predict(model3_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 12, 0.01),
+  Platinum3 = predict(model3_longit, re.form = NA, newdata = data.frame(time = seq(0, 12, 0.01),
                                                                         therapy_received = 'Platinum'), level = 0),
-  Platinum4 = predict(model4_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 12, 0.01),
+  Platinum4 = predict(model4_longit, re.form = NA, newdata = data.frame(time = seq(0, 12, 0.01),
                                                                         therapy_received = 'Platinum'), level = 0),
-  Platinum5 = predict(model5_longit, re.form = NA, newdata = data.frame(months_in_followup = seq(0, 12, 0.01),
+  Platinum5 = predict(model5_longit, re.form = NA, newdata = data.frame(time = seq(0, 12, 0.01),
                                                                         therapy_received = 'Platinum'), level = 0),
   therapy_received = 'Platinum') 
 
-meanmm_treat <- ggplot(data = psa_long_train, aes(x = months_in_followup, y = log2PSA)) + 
+meanmm_treat <- ggplot(data = psa_long_train, aes(x = time, y = log2PSA)) + 
   facet_wrap(~ factor(therapy_received, levels = c('ARSi', 'Taxane', 'PARPi', 'Platinum'))) +
   geom_line(aes(group = id_num, colour = therapy_received)) +
   geom_smooth(aes(linetype = 'dotted'), se = F, colour = 'black') +
@@ -281,9 +282,9 @@ time_var_plot <- vector(mode = 'list', length = 15)
 for(i in 1:length(models_joint)){
   x_times <- seq(0.001, 18, length = 500)
   
-  if(i <= 5) X <- cbind(1, ns(x_times, k = c(4), B = c(0, 35.5)))
-  if(i > 5 & i <= 10) X <- cbind(1, ns(x_times, k = c(2, 4), B = c(0, 35.5)))
-  if(i > 10) X <- cbind(1, ns(x_times, k = c(2, 4, 6), B = c(0, 35.5)))
+  if(i <= 5) X <- cbind(1, ns(x_times, k = c(4), B = c(0, 18.5)))
+  if(i > 5 & i <= 10) X <- cbind(1, ns(x_times, k = c(2, 4), B = c(0, 18.5)))
+  if(i > 10) X <- cbind(1, ns(x_times, k = c(2, 4, 6), B = c(0, 18.5)))
   
   mcmc_alphas <- do.call('rbind', models_joint[[i]]$mcmc$alphas)
   log_hr <- X %*% t(mcmc_alphas)
@@ -291,16 +292,16 @@ for(i in 1:length(models_joint)){
   log_hr_low <- apply(log_hr, 1, quantile, probs = 0.025)
   log_hr_upp <- apply(log_hr, 1, quantile, probs = 0.975)
   
-  time_var_plot.df <- data.frame('months_in_followup' = x_times, 
+  time_var_plot.df <- data.frame('time' = x_times, 
                                  'HR_mean' = exp(log_hr_mean), 
                                  'HR_low' = exp(log_hr_low), 
                                  'HR_up' = exp(log_hr_upp))
   
   time_var_plot[[i]] <- ggplot(time_var_plot.df) + 
-    geom_line(aes(x = months_in_followup, y = HR_mean), colour = 'red', linewidth = 0.8) + 
-    geom_line(aes(x = months_in_followup, y = HR_low), 
+    geom_line(aes(x = time, y = HR_mean), colour = 'red', linewidth = 0.8) + 
+    geom_line(aes(x = time, y = HR_low), 
               colour = 'black', linetype = 'dashed', linewidth = 0.8) + 
-    geom_line(aes(x = months_in_followup, y = HR_up), 
+    geom_line(aes(x = time, y = HR_up), 
               colour = 'black', linetype = 'dashed', linewidth = 0.8) + 
     geom_hline(yintercept = exp(coef(models_joint_compare[[(i-1)%%5 + 1]])$association), colour = 'red', 
                linetype = 'dashed', linewidth = 0.5) + 
@@ -327,7 +328,7 @@ for (i in 1:length(plots)){
   dev.off()
 }
 
-# mixed model
+# mixed models
 plots <- list(linrel[[1]], linrel[[2]], linrel[[3]], linrel[[4]], linrel[[5]],
               homosked[[1]], homosked[[2]], homosked[[3]], homosked[[4]], homosked[[5]],
               cooksdist[[1]], cooksdist[[2]], cooksdist[[3]], cooksdist[[4]], cooksdist[[5]],
@@ -352,24 +353,24 @@ for (i in 1:length(plots)){
   dev.off()
 }
 
-# joint model 
+# joint models 
 plots <- list(time_var_plot[[1]], time_var_plot[[2]], time_var_plot[[3]], 
               time_var_plot[[4]], time_var_plot[[5]], time_var_plot[[6]],
               time_var_plot[[7]], time_var_plot[[8]], time_var_plot[[9]], 
               time_var_plot[[10]], time_var_plot[[11]], time_var_plot[[12]],
               time_var_plot[[13]], time_var_plot[[14]], time_var_plot[[15]]) 
 
-filenames <- c('lmm_1knot_timevarying', 'mm1knot_1knot_timevarying',
-               'mm2knots_1knot_timevarying', 'mm3knots_1knot_timevarying',
-               'mmquad_1knot_timevarying',
+filenames <- c('lmm_timevarying1', 'mm1knot_timevarying1',
+               'mm2knots_timevarying1', 'mm3knots_timevarying1',
+               'mmquad_timevarying1',
                
-               'extra_time_varying_prop_hazards', 'model4_time_varying_prop_hazards',
-               'model3_time_varying_prop_hazards', 'model2_time_varying_prop_hazards',
-               'model5_time_varying_prop_hazards',
+               'lmm_timevarying2', 'mm1knot_timevarying2',
+               'mm2knots_timevarying2', 'mm3knots_timevarying2',
+               'mmquad_timevarying2',
                
-               'lmm_3knots_timevarying', 'mm1knot_3knots_timevarying',
-               'mm2knots_3knots_timevarying', 'mm3knots_time_varying_prop_hazards',
-               'mmquad_3knots_timevarying')
+               'lmm_timevarying3', 'mm1knot_timevarying3',
+               'mm2knots_timevarying3', 'mm3knots_timevarying3',
+               'mmquad_timevarying3')
 
 for (i in 1:length(plots)){  
   file_name = paste('C:/Users/lanbro/Documents/Figures/Q2/joint/', filenames[i], '.pdf', sep='')
