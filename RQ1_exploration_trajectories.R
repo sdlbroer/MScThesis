b03c2a08336207a893dc9d943e3694134465dfab
user1803158 <101558304+user1803158@users.noreply.github.com>
2023-12-17 16:12:24 +0100

    Update RQ1_exploration_trajectories.R

--- RQ1_exploration_trajectories.R
+++ RQ1_exploration_trajectories.R
@@ -3,7 +3,7 @@
 ###########################
 
 # load data
-source('C:/Users/lanbro/Documents/Scripts/data_management_RQ2.R')
+source('C:/Users/lanbro/OneDrive - Karolinska Institutet/Dokument/Scripts/data_management_RQ2.R')
 
 # load libraries
 library('ggpubr')
