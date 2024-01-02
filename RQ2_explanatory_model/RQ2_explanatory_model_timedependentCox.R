c1f2cee8ba084d0cec2996e44474a8e09bb28859
user1803158 <101558304+user1803158@users.noreply.github.com>
2024-01-02 11:46:39 +0100

    Update RQ2_explanatory_model_timedependentCox.R

--- RQ2_explanatory_model/RQ2_explanatory_model_timedependentCox.R
+++ RQ2_explanatory_model/RQ2_explanatory_model_timedependentCox.R
@@ -3,7 +3,7 @@
 ###########################
 
 # load data
-source('C:/Users/lanbro/OneDrive - Karolinska Institutet/Dokument/Scripts/RQ2_data_management.R')
+source('C:/Users/lanbro/OneDrive - Karolinska Institutet/Dokument/Scripts/data_management.R')
 
 # load libraries 
 library(survival) # to fit the Cox model
