034fa7fda94cc18c4da555b40311b87b61ff1df8
user1803158 <101558304+user1803158@users.noreply.github.com>
2024-01-17 10:06:12 +0100

    Update pencal.R

--- RQ3_predicting_NLCB/multiple_baseline_cov/pencal.R
+++ RQ3_predicting_NLCB/multiple_baseline_cov/pencal.R
@@ -22,20 +22,19 @@ tn <- 10 # maximum prediction time
 times <- seq(t0, tn, length.out = (tn-t0)*2+1) # number of predictions 
 n.folds <- 5 # number of folds in the cross-validation
 n.RCV <- 20 # number of repeats of the cross-validation
-seed <- floor(1803158/204) # set seed 
+seed <- floor(1803158/201) # set seed 
 seeds <- seed:(seed + n.RCV) # create new seed for each repetition
 
-# add baseline date to longitudinal measurements 
+# add baseline date to psa_long dataframe
 longdata <- psa_long_train %>%
   filter(!is.na(PSA)) %>% 
   left_join(select(psa_long_train[psa_long_train$visit == 'Treatment start',], patientId, 
-                   baseline_date = date_lab), by = 'patientId') %>%
-  distinct(id = id_num, fup_time = time, log2PSA, therapy_received) %>% 
-  mutate(therapy_received = as.factor(as.character(therapy_received)))
+                   baseline_date = date_lab), by = 'patientId')  %>%
+  select(id_num, therapy_received, fup_time = time, log2PSA, NLCB, time_obs)
 
 # create dataframe with only the survival data 
-survdata <- baseline_data_table1_train[baseline_data_table1_train$id_num %in% longdata$id,] %>%
-  distinct(id = id_num, therapy_received, event = NLCB_overall_num, time = time_obs, age,
+survdata <- baseline_data_table1[baseline_data_table1$id_num %in% longdata$id_num,] %>%
+  distinct(id_num, therapy_received, NLCB_overall_num, time_obs, age,
            Gleason, LocationMetastases, ecog, treatmentline, RT, ST) %>%
   mutate(therapy_received = as.factor(as.character(therapy_received)),
          Gleason = as.factor(as.character(Gleason)),
@@ -48,12 +47,24 @@ survdata <- baseline_data_table1_train[baseline_data_table1_train$id_num %in% lo
   filter(!is.na(treatmentline))
 
 # perform strict landmarking 
-survdata <- survdata[complete.cases(survdata) & survdata$time > t0,]
-longdata <- longdata[longdata$fup_time <= t0 & longdata$id %in% survdata$id,]
+surv_strict_landmarked <- survdata[complete.cases(survdata) & survdata$time_obs > t0,]
+long_strict_landmarked <- longdata[longdata$fup_time <= t0 & longdata$id_num %in% survdata$id_num,]
+
+# make time-varying information LOCF
+long_strict_landmarked <- long_strict_landmarked %>%
+  arrange(id_num, desc(fup_time)) %>%
+  filter(!duplicated(id_num))
+
+surv_strict_landmarked <- left_join(surv_strict_landmarked,
+                                    select(long_strict_landmarked, id_num, log2PSA),
+                                    by = 'id_num')
 
 ##########################
 ### dynamic prediction ###
 ##########################
+
+# predictors
+prednames <- c('therapy_received', 'Gleason', 'LocationMetastases', 'ecog', 'treatmentline', 'log2PSA')
 
 # m-times k-fold cross-validation
 #all_auc <- all_brier <- vector('list', n.RCV)
@@ -69,15 +80,7 @@ for(m in 1:n.RCV){
   set.seed(seeds[m])
   
   # create k random folds
-  folds_long <- create_folds(longdata, V = n.folds, id_var = 'id', seed = seeds[m])
-  ## survival data
-  folds_surv <- list('training' = vector('list', n.folds), 
-                     'testing' = vector('list', n.folds))
-  for(i in 1:n.folds){
-    ids <- unique(folds_long$training[[i]]$id)
-    folds_surv$training[[i]] <- survdata[survdata$id %in% ids,]
-    folds_surv$testing[[i]] <- survdata[!(survdata$id %in% ids),]
-  }
+  folds_surv <- create_folds(surv_strict_landmarked, V = n.folds, id_var = 'id_num', seed = seeds[m])
   
   # perform prediction k times
   auc_df <- brier_df <- as.data.frame(times)
@@ -87,70 +90,51 @@ for(m in 1:n.RCV){
     print(paste0('Repetition ', m, ', fold ', i))
     
     # train and test set
-    df_train_long <- folds_long$training[[i]]
     df_train_surv <- folds_surv$training[[i]]
-    df_test_long <- folds_long$testing[[i]]
     df_test_surv <- folds_surv$testing[[i]]
-
-    # fit models 
-    ## step 1: estimate mixed-effects model
-    model_lme <- fit_lmms(y.names = c('log2PSA'),
-                          fixefs = ~ therapy_received*fup_time, 
-                          ranefs = ~ fup_time | id,
-                          long.data = df_train_long, surv.data = df_train_surv,
-                          t.from.base = fup_time,
-                          n.boots = 0, n.cores = 8, verbose = F)
-    ## step 2: compute predicted random effects
-    sum_lme <- summarize_lmms(object = model_lme, n.cores = 8, verbose = F)
-    ## step 3: estimate penalized Cox model
-    model_pcr <- fit_prclmm(object = sum_lme, surv.data = df_train_surv,
-                            baseline.covs = ~ therapy_received + Gleason + LocationMetastases + ecog + treatmentline,
-                            penalty = 'ridge', standardize = F,
-                            n.cores = 8, verbose = F)
     
-    # validate model on test set
+    # fit landmark model (Cox PH)
+    model_landmark_LOCF <- coxph(Surv(time_obs, NLCB_overall_num) ~ therapy_received + Gleason + LocationMetastases + ecog + treatmentline + log2PSA,
+                                 data = df_train_surv, x = TRUE)
+    
+    
+    # validate model on test set 
     ## predict survival outcome
-    surv_prob <- survpred_prclmm(step1 = model_lme, step2 = sum_lme, 
-                                 step3 = model_pcr, times = times,
-                                 new.longdata = df_test_long,
-                                 new.basecovs = df_test_surv)
-    fail_prob <- 1 - surv_prob$predicted_survival[,2:ncol(surv_prob$predicted_survival)]
-    colnames(fail_prob) <- times
-    
-    # predictive performance
-    acc_measures <- Score(as.list(fail_prob), 
-                          formula = Surv(time, event) ~ 1, 
-                          data = df_test_surv,
-                          times = times, 
-                          cens.model = 'km',
-                          metrics = c('auc','brier'), 
-                          conf.int = FALSE, 
-                          exact = FALSE, 
-                          split.method	= 'none', 
-                          B = 0)
-    ## extract AUC
-    auc <- acc_measures$AUC$score
-    auc <- auc[auc$model == auc$times,-1]
-    ## extract Brier
-    brier <- acc_measures$Brier$score
-    brier <- brier[brier$model == brier$times,-1]
-    ## safe accuracy measures
-    auc_df <- left_join(auc_df, auc, by = 'times')
-    brier_df <- left_join(brier_df, brier, by = 'times')
-    
-    # C-index
-    c_index <- concordance.index(x = fail_prob[,2], method = 'noether',
-                                 surv.time = df_test_surv$time, 
-                                 surv.event = df_test_surv$event)$c.index
-    ## save all C-indexes
-    all_cindex[m,i] <- c_index
+    fail_prob <- try(as.data.frame(1 - predictSurvProb(model_landmark_LOCF, newdata = df_test_surv[,prednames], times = times)), silent = TRUE)
+    if(inherits(fail_prob, "try-error")) print(paste0('For repetition ', m, ', fold ', i, ': the survival model had NA values'))
+    if(!inherits(fail_prob, "try-error")){
+      colnames(fail_prob) <- times
+      
+      # predictive performance
+      acc_measures <- Score(as.list(fail_prob),
+                            formula = Surv(time_obs, NLCB_overall_num) ~ 1, 
+                            data = df_test_surv,
+                            times = times, 
+                            cens.model = 'km',
+                            metrics = c('auc','brier'), 
+                            conf.int = FALSE, 
+                            exact = FALSE, 
+                            split.method	= 'none', 
+                            B = 0)
+      ## extract AUC
+      auc <- acc_measures$AUC$score
+      auc <- auc[auc$model == auc$times,-1]
+      ## extract Brier
+      brier <- acc_measures$Brier$score
+      brier <- brier[brier$model == brier$times,-1]
+      ## safe accuracy measures
+      auc_df <- left_join(auc_df, auc, by = 'times')
+      brier_df <- left_join(brier_df, brier, by = 'times')
+      
+      # C-index
+      c_index <- concordance.index(x = fail_prob[,2], method = 'noether',
+                                   surv.time = df_test_surv$time_obs, 
+                                   surv.event = df_test_surv$NLCB_overall_num)$c.index
+      ## save all C-indexes
+      all_cindex[m,i] <- c_index
+    }
   }
   # summarize predictive performance
-  ## change row/column names
-  names(auc_df) <- c('times', paste0(rep('fold', n.folds), 1:n.folds))
-  rownames(auc_df) <- auc_df[,1]
-  names(brier_df) <- c('times', paste0(rep('fold', n.folds), 1:n.folds))
-  rownames(brier_df) <- brier_df[,1]
   ## calculate mean performance
   auc_df$mean_auc <- apply(auc_df[,-1], 1, mean, na.rm = T)
   brier_df$mean_brier <- apply(brier_df[,-1], 1, mean, na.rm = T)
@@ -162,15 +146,11 @@ for(m in 1:n.RCV){
   brier_reps <- left_join(brier_reps, brier_df[,c('times', 'mean_brier')], by = 'times')
   
   # clean up environment
-  rm(auc_df, brier_df, auc, brier, acc_measures, fail_prob, surv_prob,
-     df_train_long, df_train_surv, df_test_long, df_test_surv, 
-     model_lme, sum_lme, model_pcr)
+  rm(auc_df, brier_df, auc, brier, acc_measures, fail_prob,
+     df_train_surv, df_test_surv)
 }
 
 # summarize predictive performance
-## change row/column names
-names(auc_reps) <- c('times', paste0(rep('repetition', n.RCV), 1:n.RCV))
-names(brier_reps) <- c('times', paste0(rep('repetition', n.RCV), 1:n.RCV))
 ## calculate mean predictive performance of all RCV
 auc_reps$mean_auc <- apply(auc_reps[,-1], 1, mean, na.rm = T)
 brier_reps$mean_brier <- apply(brier_reps[,-1], 1, mean, na.rm = T)
