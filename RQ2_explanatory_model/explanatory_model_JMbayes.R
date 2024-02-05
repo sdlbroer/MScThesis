6efe023d8538a60b78538c1eeb09ec50e79c818f
user1803158 <101558304+user1803158@users.noreply.github.com>
2024-02-05 12:33:53 +0100

    Update explanatory_model_JMbayes.R

--- RQ2_explanatory_model/explanatory_model_JMbayes.R
+++ RQ2_explanatory_model/explanatory_model_JMbayes.R
@@ -3,7 +3,7 @@
 ###########################
 
 # load data
-source('C:/Users/lanbro/OneDrive - Karolinska Institutet/Dokument/Scripts/data_management.R')
+source('C:/Users/lanbro/OneDrive - Karolinska Institutet/Dokument/Scripts/data_management_RQ2.R')
 
 # load libraries 
 library(nlme) # fit the mixed model
